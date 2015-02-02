09fceaae86a590694260a4b766771817d878c7b6
Jeff Hsu <jeffhsu@yahoo-inc.com>
2015-02-01 22:36:36 -0800

    adding email sharing feature

--- app/src/main/java/com/example/jeffhsu/gridimagesearch/activities/ImageDisplayActivity.java
+++ app/src/main/java/com/example/jeffhsu/gridimagesearch/activities/ImageDisplayActivity.java
@@ -1,15 +1,29 @@
 package com.example.jeffhsu.gridimagesearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.jeffhsu.gridimagesearch.R;
 import com.example.jeffhsu.gridimagesearch.models.ImageResult;
 import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class ImageDisplayActivity extends ActionBarActivity {
 
@@ -18,7 +32,7 @@ public class ImageDisplayActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_display);
         // Remove actionbar
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
         // Pull out the url from the intent
         // String url = getIntent().getStringExtra("url");
@@ -45,10 +59,57 @@ public class ImageDisplayActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.barSharing) {
+            Toast.makeText(this,"Clicked!", Toast.LENGTH_LONG).show();
+//            ImageView ivImage = (ImageView) findViewById(R.id.ivImageResult);
+            onShareItem();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onShareItem() {
+        // Get access to bitmap image from view
+        ImageView ivImage = (ImageView) findViewById(R.id.ivImageResult);
+        // Get access to the URI for the bitmap
+        Uri bmpUri = getLocalBitmapUri(ivImage);
+        if (bmpUri != null) {
+            // Construct a ShareIntent with link to image
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+            shareIntent.setType("image/*");
+            // Launch sharing dialog for image
+            startActivity(Intent.createChooser(shareIntent, "Share Image"));
+        } else {
+            // ...sharing failed, handle error
+        }
+    }
+
+    // Returns the URI path to the Bitmap displayed in specified ImageView
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file =  new File(Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 }
